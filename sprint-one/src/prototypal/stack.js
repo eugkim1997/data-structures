50877aa4a81d8630a72383237908ed7625cf7f59
Eugene Kim <eugkim1997@gmail.com>
2020-07-01 15:58:52 -0700

    Implemented prototypal & pseudoclassical instantiation styles

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = Object.create(stackMethods);
+  newStack.storage = {};
+  newStack.n = 0;
+
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.n] = value;
+  this.n++;
+};
 
+stackMethods.pop = function() {
+  if (this.n > 0) {
+    this.n--;
+    var result = this.storage[this.n];
+    delete this.storage[this.n];
+    return result;
+  }
+};
+
+stackMethods.size = function() {
+  return this.n;
+};
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.n = 0;
 };
+
+Stack.prototype.push = function(value) {
+  this.storage[this.n] = value;
+  this.n++;
+};
+
+Stack.prototype.pop = function() {
+  if (this.n > 0) {
+    this.n--;
+    var result = this.storage[this.n];
+    delete this.storage[this.n];
+    return result;
+  }
+};
+
+Stack.prototype.size = function() {
+  return this.n;
+}
 
 
