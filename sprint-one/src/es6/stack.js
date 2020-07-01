f03ad2113f83ac92a753fe0b1016550616b67366
Eugene Kim <eugkim1997@gmail.com>
2020-07-01 16:26:48 -0700

    Implemented ES6 instantiation style

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,26 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.n = 0;
+  }
+
+  push(value) {
+    this.storage[this.n] = value;
+    this.n++;
+  }
+
+  pop() {
+    if (this.n > 0) {
+      this.n--;
+      var result = this.storage[this.n];
+      delete this.storage[this.n];
+      return result;
+    }
+  }
+
+  size() {
+    return this.n;
   }
 
 }
