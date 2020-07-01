f34e148e1425444c21bf0f5e57843a2fff4a4764
Eugene Kim <eugkim1997@gmail.com>
2020-07-01 15:13:03 -0700

    Implemented stack & queue with functional-shared instantiation

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,30 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newQueue = {};
+  newQueue.storage = {};
+  newQueue.start = 0;
+  newQueue.end = 0;
+  _.extend(newQueue, queueMethods);
+  return newQueue;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function(value) {
+  this.storage[this.end] = value;
+  this.end++;
+}
 
+queueMethods.dequeue = function() {
+  if (this.end > this.start) {
+    var result = this.storage[this.start];
+    delete this.storage[this.start];
+    this.start++;
+    return result;
+  }
+}
+
+queueMethods.size = function() {
+  return this.end - this.start;
+}
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,29 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var newStack = {};
+  newStack.storage = {};
+  newStack.n = 0;
+  _.extend(newStack, stackMethods);
+  return newStack;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function(value) {
+  this.storage[this.n] = value;
+  this.n++;
+};
 
+stackMethods.pop = function() {
+  if (this.n > 0) {
+    this.n--;
+    var result = this.storage[this.n];
+    delete this.storage[this.n];
+    return result;
+  }
+};
+
+stackMethods.size = function() {
+  return this.n;
+};
