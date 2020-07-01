b2158aa528f6a3cac7a557a2017062741c56e100
Eugene Kim <eugkim1997@gmail.com>
2020-07-01 11:52:49 -0700

    Implemented stack with functional instantiation

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -3,15 +3,25 @@ var Stack = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var n = 0;
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[n] = value;
+    n++;
   };
 
   someInstance.pop = function() {
+    if (n > 0) {
+      n -= 1;
+      var result = storage[n];
+      delete storage[n]
+      return result;
+    }
   };
 
   someInstance.size = function() {
+    return n;
   };
 
   return someInstance;
